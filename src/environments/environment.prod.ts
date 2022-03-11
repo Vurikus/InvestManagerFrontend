7058e541cccf969b3d4720441eb9f40530e2e163
iusoltsev <iusoltsev@moysklad.ru>
2022-03-11 19:46:05 +0300

    Add data field in report form

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apiUrl: 'http://localhost:28081',
+  apiUrl: 'http://localhost:28082',
   cbrApi: 'https://www.cbr-xml-daily.ru/daily_json.js',
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  apiUrl: 'http://localhost:28081',
-  wsUrl: 'http://localhost:28081',
+  apiUrl: 'http://localhost:28082',
+  wsUrl: 'http://localhost:28082',
   cbrApi: 'https://www.cbr-xml-daily.ru/daily_json.js',
   production: false
 };
